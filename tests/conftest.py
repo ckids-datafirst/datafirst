f3c96301c268255647ec68cd7b9758bac052fe87
Maximiliano Osorio <maxiosorio@gmail.com>
2023-10-11 19:21:12 -0300

    fix: use classes

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,17 +4,15 @@ from pathlib import Path
 
 import pytest
 
-from datafirst.database import close_database, get_cursor, open_database
+from datafirst.database import Database
 
 RESOURCES_DIRECTORY = Path(__file__).parent / "resources"
 DATABASE_PATH = RESOURCES_DIRECTORY / "datafirst.sqlite"
 
 
 @pytest.fixture
-def cursor() -> sqlite3.Cursor:
+def database() -> Database:
     if not os.path.exists(DATABASE_PATH):
         raise FileNotFoundError("Database not found " + str(DATABASE_PATH))
-    connection = open_database(DATABASE_PATH)
-    cursor = get_cursor(connection)
-    yield cursor
-    close_database(connection)
+    db = Database(DATABASE_PATH)
+    return db
--- tests/test_database.py
+++ tests/test_database.py
@@ -2,19 +2,7 @@ import sqlite3
 
 import pytest
 
-from datafirst.database import (
-    get_advisors,
-    get_advisors_by_project_id,
-    get_awards_by_project_id,
-    get_projects,
-    get_projects_by_advisor_id,
-    get_projects_by_student_id,
-    get_semester_advisor_by_id,
-    get_skills_by_project_id,
-    get_students,
-    get_students_by_project_id,
-    get_topics_by_project_id,
-)
+from datafirst.database import Database
 
 advisor_id = "advisor1@usc.edu"
 project_id = "2023-test-project1"
@@ -23,45 +11,45 @@ school_id = 1
 topic_id = 1
 
 
-def test_get_skills_by_project_id(cursor: sqlite3.Cursor):
-    get_skills_by_project_id(cursor, project_id=project_id)
+def test_get_skills_by_project_id(database: Database):
+    database.get_skills_by_project_id(project_id=project_id)
 
 
-def test_get_topics_by_project_id(cursor: sqlite3.Cursor):
-    get_topics_by_project_id(cursor, project_id=project_id)
+def test_get_topics_by_project_id(database: Database):
+    database.get_topics_by_project_id(project_id=project_id)
 
 
-def test_get_awards_by_project_id(cursor: sqlite3.Cursor):
-    get_awards_by_project_id(cursor, project_id=project_id)
+def test_get_awards_by_project_id(database: Database):
+    database.get_awards_by_project_id(project_id=project_id)
 
 
-def test_get_students_by_project_id(cursor: sqlite3.Cursor):
-    get_students_by_project_id(cursor, project_id=project_id)
+def test_get_students_by_project_id(database: Database):
+    database.get_students_by_project_id(project_id=project_id)
 
 
-def test_get_advisors_by_project_id(cursor: sqlite3.Cursor):
-    get_advisors_by_project_id(cursor, project_id=project_id)
+def test_get_advisors_by_project_id(database: Database):
+    database.get_advisors_by_project_id(project_id=project_id)
 
 
-def test_get_projects(cursor: sqlite3.Cursor):
-    get_projects(cursor)
+def test_get_projects(database: Database):
+    database.get_projects()
 
 
-def test_get_projects_by_student_id(cursor: sqlite3.Cursor):
-    get_projects_by_student_id(cursor, 1)
+def test_get_projects_by_student_id(database: Database):
+    database.get_projects_by_student_id(1)
 
 
-def test_get_projects_by_advisor_id(cursor: sqlite3.Cursor):
-    get_projects_by_advisor_id(cursor, advisor_id)
+def test_get_projects_by_advisor_id(database: Database):
+    database.get_projects_by_advisor_id(advisor_id)
 
 
-def test_get_advisors(cursor: sqlite3.Cursor):
-    get_advisors(cursor)
+def test_get_advisors(database: Database):
+    database.get_advisors()
 
 
-def test_get_students(cursor: sqlite3.Cursor):
-    get_students(cursor)
+def test_get_students(database: Database):
+    database.get_students()
 
 
-def test_get_semester_advisor(cursor: sqlite3.Cursor):
-    get_semester_advisor_by_id(cursor, advisor_id)
+def test_get_semester_advisor(database: Database):
+    database.get_semester_advisor_by_id(advisor_id)
